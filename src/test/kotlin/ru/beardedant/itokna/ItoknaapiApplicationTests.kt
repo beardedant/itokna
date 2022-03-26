e6140c03fffe46bca2bcf0050014555d67bbf1d8
beardedant <theant@mail.ru>
2022-03-26 21:51:19 +0400

    initial commit

---
+++ src/test/kotlin/ru/beardedant/itokna/ItoknaapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.beardedant.itokna
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ItoknaapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
